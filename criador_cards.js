9337ef1cc1c06eb6b42d785bd7c23375aaad82b6
GregorioFornetti <fornetti.greg@gmail.com>
2021-02-20 13:47:49 -0300

    first commit
    Projeto "pronto"

---
+++ criador_cards.js
@@ -0,0 +1,144 @@
+
+var card_materia_atual
+var card_tarefa_atual
+
+function criar_card_materia(titulo_materia) {
+    let card = criar_estrutura_card()
+    let card_header = card.children[0]
+    let card_body = card.children[1]
+    let card_footer = card.children[2]
+    let card_box = document.createElement("div")
+    card_box.dataset.nome_materia = titulo_materia
+    card_body.dataset.nome_materia = titulo_materia
+
+    let title = document.createElement("h2")
+    title.className = "text-white"
+    title.innerText = titulo_materia
+
+    card_header.className += ' bg-dark'
+    card_header.appendChild(title)
+
+    let botao_tarefa = criar_botao_card_materia("Criar nova tarefa", "btn-success")
+    botao_tarefa.dataset.bsToggle = "modal"
+    botao_tarefa.dataset.bsTarget = "#modal-criar-tarefa"
+    botao_tarefa.addEventListener("click", () => {
+        card_materia_atual = card_box
+    })
+
+    let botao_apagar = criar_botao_card_materia("Apagar metéria", "btn-danger")
+    botao_apagar.dataset.bsToggle = "modal"
+    botao_apagar.dataset.bsTarget = "#modal-apagar-materia"
+    botao_apagar.addEventListener("click", () => {
+        card_tarefa_atual = card_box
+    })
+
+    card_footer.className += ' bg-dark'
+    card_footer.append(botao_tarefa, botao_apagar)
+
+    card_box.append(card)
+    return card_box
+}
+
+
+function criar_card_tarefa(titulo_tarefa, info_tarefa, concluida) {
+    let card = criar_estrutura_card()
+    let card_header = card.children[0]
+    let card_body = card.children[1]
+    let card_footer = card.children[2]
+
+    card.dataset.concluida = concluida
+
+    card_header.className += ' h5 pt-1'
+    card_header.innerText = titulo_tarefa
+
+    card_body.innerText = info_tarefa
+
+    let botao_concluida = criar_botao_card_tarefa("Marcar como concluida", "btn-success")
+    botao_concluida.addEventListener("click", () => {
+        let nome_materia = card.parentNode.dataset.nome_materia
+        let lista_tarefas_JSON = JSON.parse(localStorage.getItem(nome_materia))
+        let indice_tarefa_atual = coletar_indice_card_tarefa(card)
+
+        if (card.dataset.concluida == 'false') {
+            card.dataset.concluida = true
+            lista_tarefas_JSON[indice_tarefa_atual].concluida = true
+            card_header.className += ' bg-success text-white'
+            botao_concluida.innerText = "Desmarcar como concluida"
+        }
+        else {
+            card.dataset.concluida = false
+            lista_tarefas_JSON[indice_tarefa_atual].concluida = false
+            card_header.className = card_header.className.replace(" bg-success text-white", "")
+            botao_concluida.innerText = "Marcar como concluida"
+        }
+        localStorage.setItem(nome_materia, JSON.stringify(lista_tarefas_JSON))
+    })
+    if (concluida) {
+        card_header.className += ' bg-success text-white'
+        botao_concluida.innerText = "Desmarcar como concluida"
+    }
+
+    let botao_editar = criar_botao_card_tarefa("Editar tarefa", "btn-primary")
+    botao_editar.dataset.bsToggle = "modal"
+    botao_editar.dataset.bsTarget = "#modal-editar-tarefa"
+    botao_editar.addEventListener("click", () => {
+        card_tarefa_atual = card
+        document.querySelector("#input-editar-nome-tarefa").value = titulo_tarefa
+        document.querySelector("#info-editar-tarefa").value = info_tarefa
+    })
+
+    let botao_apagar = criar_botao_card_tarefa("Apagar tarefa", "btn-danger")
+    botao_apagar.addEventListener("click", () => {
+        let nome_materia = card.parentNode.dataset.nome_materia
+        let lista_tarefas_JSON = JSON.parse(localStorage.getItem(nome_materia))
+        let indice_tarefa_atual = coletar_indice_card_tarefa(card)
+
+        lista_tarefas_JSON.splice(indice_tarefa_atual, 1)
+        localStorage.setItem(nome_materia, JSON.stringify(lista_tarefas_JSON))
+
+        card.remove() 
+    })
+
+    card_footer.append(botao_concluida, botao_editar, botao_apagar)
+
+    card.className += ' mb-3'
+    return card
+}
+
+
+function criar_estrutura_card() {
+    let card = document.createElement("div")
+    card.className = "card"
+
+    let card_header = document.createElement("div")
+    card_header.className = "card-header"
+    let card_body = document.createElement("div")
+    card_body.className = "card-body"
+    let card_footer = document.createElement("div")
+    card_footer.className = "card-footer"
+    
+    card.append(card_header, card_body, card_footer)
+    return card
+}
+
+function criar_botao_card_tarefa(texto_botao, classe_cor) {
+    let botao = document.createElement("button")
+    botao.className = `btn ${classe_cor} mb-2 me-2`
+    botao.innerText = texto_botao
+    return botao
+}
+
+function criar_botao_card_materia(texto_botao, classe_cor) {
+    let botao = document.createElement("button")
+    botao.className = `btn ${classe_cor} mb-2 me-3`
+    botao.innerText = texto_botao
+    return botao
+}
+
+function coletar_indice_card_tarefa(card_tarefa) {
+    let lista_elementos_tarefa = card_tarefa.parentNode.children
+
+    for (let i = 0; i < lista_elementos_tarefa.length; i++) 
+        if (lista_elementos_tarefa[i].isSameNode(card_tarefa)) 
+            return i
+}
---
+++ main.js
@@ -0,0 +1,134 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    let main_div = document.querySelector("#main-div")
+    let botao_criar_materia = document.querySelector("#btn-criar-materia")
+    let botao_criar_tarefa = document.querySelector("#btn-criar-tarefa")
+    let botao_apagar_materia = document.querySelector("#btn-apagar-materia")
+    let botao_editar_tarefa = document.querySelector("#btn-editar-tarefa")
+    let botao_remover_tarefas_prontas = document.querySelector("#btn-remover-tarefas-prontas")
+
+    botao_criar_materia.addEventListener("click", () => {
+        let input_nome_materia = document.querySelector("#input-nome-materia")
+        if (input_nome_materia.value.length > 0) {
+            localStorage.setItem(input_nome_materia.value, "[]")
+            colocar_card(main_div, criar_card_materia(input_nome_materia.value))
+            input_nome_materia.value = ""
+            alert("Matéria criada com sucesso !")
+        }
+        else {
+            alert("Digite o nome da matéria !")
+        }
+    })
+
+    botao_criar_tarefa.addEventListener("click", () => {
+        let input_titulo_tarefa = document.querySelector("#input-criar-nome-tarefa")
+        let input_info_tarefa = document.querySelector("#info-criar-tarefa")
+        if (input_titulo_tarefa.value.length > 0 && input_info_tarefa.value.length > 0) {
+            let nome_materia = card_materia_atual.dataset.nome_materia
+            let card_body = card_materia_atual.children[0].children[1]
+            let lista_tarefas = JSON.parse(localStorage.getItem(nome_materia))
+
+            lista_tarefas.push({
+                "titulo" : input_titulo_tarefa.value,
+                "info" : input_info_tarefa.value,
+                "concluida" : false
+            })
+            localStorage.setItem(nome_materia, JSON.stringify(lista_tarefas))
+            card_body.append(criar_card_tarefa(input_titulo_tarefa.value, input_info_tarefa.value, false))
+
+            input_titulo_tarefa.value = ''
+            input_info_tarefa.value = ''
+            alert("Tarefa adicionada com sucesso !")
+        }
+        else {
+            alert("Preencha todos os dados !")
+        }
+    })
+
+    botao_apagar_materia.addEventListener("click", () => {
+        let nome_materia = card_tarefa_atual.dataset.nome_materia
+        localStorage.removeItem(nome_materia)
+        card_tarefa_atual.remove()
+        alert(`Matéria "${nome_materia}" removida com sucesso !`)
+    })
+
+    botao_editar_tarefa.addEventListener("click", () => {
+        let input_titulo_tarefa = document.querySelector("#input-editar-nome-tarefa")
+        let input_info_tarefa = document.querySelector("#info-editar-tarefa")
+        let nome_materia = card_tarefa_atual.parentNode.dataset.nome_materia
+        let lista_tarefas_JSON = JSON.parse(localStorage.getItem(nome_materia))
+        let indice_tarefa_atual = coletar_indice_card_tarefa(card_tarefa_atual)
+
+        if (input_titulo_tarefa.value.length > 0 && input_info_tarefa.value.length > 0) {
+            card_tarefa_atual.children[0].innerText = input_titulo_tarefa.value
+            card_tarefa_atual.children[1].innerText = input_info_tarefa.value
+
+            lista_tarefas_JSON[indice_tarefa_atual] = {
+                "titulo" : input_titulo_tarefa.value,
+                "info" : input_info_tarefa.value,
+                "concluida" : card_tarefa_atual.dataset.concluida
+            }
+            localStorage.setItem(nome_materia, JSON.stringify(lista_tarefas_JSON))
+            alert("Tarefa editada com sucesso !")
+        }
+        else {
+            alert("Preencha todos os dados !")
+        }
+    })
+
+    botao_remover_tarefas_prontas.addEventListener("click", () => {
+        for (let nome_materia in localStorage) {
+            if (localStorage.hasOwnProperty(nome_materia)) {
+                let card_body_materia
+                for (let card_number in main_div.children) {
+                    if (main_div.children[card_number].dataset.nome_materia === nome_materia) {
+                        card_body_materia = main_div.children[card_number].children[0].children[1]
+                        break
+                    }
+                }
+                
+                let lista_tarefas = JSON.parse(localStorage.getItem(nome_materia))
+                for (let i = 0; i < lista_tarefas.length; i++) {
+                    if (lista_tarefas[i].concluida) {
+                        lista_tarefas.splice(i, 1)
+                        card_body_materia.children[i].remove()
+                        i--
+                    }
+                }
+                localStorage.setItem(nome_materia, JSON.stringify(lista_tarefas))
+            }
+        }
+    })
+
+    // Criar todas matérias e tarefas armazenadas no navegador.
+    for (let nome_materia in localStorage) {
+        if (localStorage.hasOwnProperty(nome_materia)) {
+            let materia_atual = criar_card_materia(nome_materia)
+            let lista_tarefas = JSON.parse(localStorage.getItem(materia_atual.dataset.nome_materia))
+            let card_body = materia_atual.children[0].children[1]
+            
+            for (let i = 0; i < lista_tarefas.length; i++) {
+                let tarefa = lista_tarefas[i]
+                let card_tarefa = criar_card_tarefa(tarefa.titulo, tarefa.info, tarefa.concluida)
+                card_body.append(card_tarefa)
+            }
+
+            colocar_card(main_div, materia_atual)
+        }
+    }
+
+})
+
+function colocar_card(main_div, nova_card) {
+    // Adiciona a card na div principal respeitando ordem alfabética de nome de matérias.
+    for (let i = 0; i < main_div.children.length; i++) {
+        let materia_atual = main_div.children[i]
+        let nome_materia_atual = materia_atual.dataset.nome_materia
+        if (nova_card.dataset.nome_materia < nome_materia_atual) {
+            materia_atual.insertAdjacentElement("beforebegin", nova_card)
+            return
+        }
+    }
+    main_div.append(nova_card)
+}
